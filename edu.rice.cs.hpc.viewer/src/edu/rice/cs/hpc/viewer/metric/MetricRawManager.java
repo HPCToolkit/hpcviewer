68b9885bc82b8591e448992b1e6a637801f0c835
laksono@gmail.com <laksono@gmail.com>
2020-05-27 14:18:02 -0500

    hpcviewer: draft support for "invisible" metrics
    hpcprof now provides 5 different types of metric's visibility:
    0: hidden
    1: show
    2: inclusive only
    3: exclusive only
    4: completely invisible (*new*)
    
    The last type is only used to compute derived metrics, and shouldn't be
    visible by users. This commit fixes hpcviewer to handle hpcprof's new
    metric type.

--- edu.rice.cs.hpc.viewer/src/edu/rice/cs/hpc/viewer/metric/MetricRawManager.java
+++ edu.rice.cs.hpc.viewer/src/edu/rice/cs/hpc/viewer/metric/MetricRawManager.java
@@ -1,12 +1,14 @@
 package edu.rice.cs.hpc.viewer.metric;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.swt.widgets.TreeColumn;
 
 import edu.rice.cs.hpc.data.experiment.metric.BaseMetric;
 import edu.rice.cs.hpc.data.experiment.metric.DerivedMetric;
 import edu.rice.cs.hpc.data.experiment.metric.IMetricManager;
+import edu.rice.cs.hpc.data.experiment.metric.BaseMetric.VisibilityType;
 import edu.rice.cs.hpc.viewer.scope.ScopeTreeViewer;
 
 /********************************************************
@@ -25,46 +27,42 @@ public class MetricRawManager implements IMetricManager
 	
 	@Override
 	public BaseMetric getMetric(String ID) {
-		BaseMetric []metrics = getMetrics();
- 		for (BaseMetric m : metrics ) {
-			if (m.getShortName().equals(ID))
-				return m;
+
+		List<BaseMetric> list = getVisibleMetrics();
+		if (list != null) {
+			for (BaseMetric m : list) {
+				if (m.getShortName().equals(ID)) 
+					return m;
+			}
 		}
 		return null;
 	}
 
 	@Override
 	public BaseMetric getMetric(int index) {
-		BaseMetric []metrics = getMetrics();
-		if (metrics != null) {
-			if (index >= 0 && index < metrics.length)
-				return metrics[index];
-		}
+
+		List<BaseMetric> list = getVisibleMetrics();
+		if (list != null)
+			return list.get(index);
 		return null;
 	}
 
 	@Override
 	public int getMetricCount() {
-		BaseMetric[] metrics = getMetrics();
-		if (metrics != null)
-			return metrics.length;
+
+		List<BaseMetric> list = getVisibleMetrics();
+		if (list != null)
+			return list.size();
 		return 0;
 	}
 
 	@Override
 	public BaseMetric[] getMetrics() {
-		TreeColumn []columns = treeViewer.getTree().getColumns();
-		if (columns != null && columns.length > 1) {
-			ArrayList<BaseMetric> listMetrics = new ArrayList<BaseMetric>(columns.length);
-			for(TreeColumn col : columns) {
-				Object obj = col.getData();
-				if (obj instanceof BaseMetric) {
-					listMetrics.add((BaseMetric) obj);
-				}
-			}
+		List<BaseMetric> listMetrics = getVisibleMetrics();
+		if (listMetrics != null && listMetrics.size()>0) {
 			BaseMetric []metrics = new BaseMetric[listMetrics.size()];
 			return listMetrics.toArray(metrics);
-		}
+		}		
 		return null;
 	}
 
@@ -76,5 +74,22 @@ public class MetricRawManager implements IMetricManager
 	public BaseMetric getMetricFromOrder(int order) {
 
 		return getMetric(order);
+	}
+
+	@Override
+	public List<BaseMetric> getVisibleMetrics() {
+		TreeColumn []columns = treeViewer.getTree().getColumns();
+		if (columns != null && columns.length > 1) {
+			ArrayList<BaseMetric> listMetrics = new ArrayList<BaseMetric>(columns.length);
+			for(TreeColumn col : columns) {
+				Object obj = col.getData();
+				if (obj instanceof BaseMetric) {
+					if ( ((BaseMetric)obj).getVisibility() != VisibilityType.INVISIBLE )
+						listMetrics.add((BaseMetric) obj);
+				}
+			}
+			return listMetrics;
+		}
+		return null;
 	}
 }
